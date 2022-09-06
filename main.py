77c151b455025472d6a7ec44880caebfb17233f3
Dewry Peña <87697718+dewrygabrielp@users.noreply.github.com>
2022-09-06 18:22:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from Fitness.Formulas.conversor import Conversor
 from Fitness.Formulas.formules import Formules
 from Fitness.Formulas.tables import women_age_range
 
-obje = Conversor()
-(obje.pregunta())
-obje.convertir_peso(obje.pregunta())
+if __name__ == "__main__":
 
-my = Formules()
-print(my.pregunta())
-print(my.genero(obje.convertir_peso(obje.pregunta())))
+#   obje = Conversor()
+#   (obje.pregunta())
+#   obje.convertir_peso(obje.pregunta())
 
-women_age_range()
+#   my = Formules()
+#   print(my.pregunta())
+#   print(my.genero(obje.convertir_peso(obje.pregunta())))
+
+#   women_age_range()
